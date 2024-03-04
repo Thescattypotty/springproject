0d020d568275e6a822cb6b1f1c68bb5de481501b
senshi <bnsyahya123@gmail.com>
2024-03-04 15:46:20 +0100

    Some changes

--- src/main/java/com/example/SpringProject/Controller/HomeController.java
+++ src/main/java/com/example/SpringProject/Controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.example.SpringProject.Controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
  
-    @RequestMapping("/")
-    String Home(){
-        return "Hello world";
+    @RequestMapping(path = "", name="Home.index")
+    String index(){
+        return "Hello Home";
+    }
+    
+    @RequestMapping(path = "about", name = "Home.about")
+    String about() {
+        return "Hello about";
+    }
+    @RequestMapping(path="users/{userId}", method = RequestMethod.GET)
+    String users(@PathVariable String userId){
+        return "Welcome User " + userId;
     }
 }
